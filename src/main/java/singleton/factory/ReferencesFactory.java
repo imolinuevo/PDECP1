5bbc4002f8e84a97647ebc3068ec73caeb3b1d57
Iñigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-05 16:55:33 +0200

    Singleton done. Iñigo Molinuevo

---
+++ src/main/java/singleton/factory/ReferencesFactory.java
@@ -0,0 +1,41 @@
+package singleton.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ReferencesFactory {
+	
+	private static ReferencesFactory referencesFactory;
+	
+	private Map<String, Integer> references;
+
+	private int reference;
+
+	private ReferencesFactory() {
+		this.references = new HashMap<>();
+		this.reference = 0;
+		ReferencesFactory.referencesFactory = null;
+	}
+	
+	public static ReferencesFactory getReferencesFactory() {
+		if (ReferencesFactory.referencesFactory == null) {
+			ReferencesFactory.referencesFactory = new ReferencesFactory();
+		}
+		return ReferencesFactory.referencesFactory;
+	}
+
+	public int getReference(String key) {
+		Integer result = this.references.get(key);
+		if (result == null) {
+			this.references.put(key, this.reference);
+			result = this.reference;
+			reference++;
+		}
+		return result;
+	}
+
+	public void removeReference(String key) {
+		this.references.remove(key);
+	}
+
+}
