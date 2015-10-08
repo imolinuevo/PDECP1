fecdbe48ab829291999c08849247c2750b390cc0
Inigo Molinuevo <i.m.parron@opendeusto.es>
2015-10-08 11:46:48 +0200

    Decorator main finished. Iñigo Molinuevo

--- src/main/java/decorator/vehiculos/GestorVehiculos.java
+++ src/main/java/decorator/vehiculos/GestorVehiculos.java
@@ -4,6 +4,12 @@ public class GestorVehiculos {
 
 	public void gestionar() {
 		Componente a3 = new Vehiculo("A3", 1000);
+		System.out.println(a3.getPrecio());
+		Componente a3m = new MP3(a3);
+		System.out.println(a3m.getPrecio());
+		System.out.println(a3m.precioFinal());
+		Componente a3mg = new GPS(a3);
+		System.out.println(a3mg.precioFinal());
 	}
 
 	public static void main(String[] args) {
